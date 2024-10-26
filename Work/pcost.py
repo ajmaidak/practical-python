5445c277cdabe9bed77719ea8c6b1c6e0c547e83
Alexander J. Maidak <amaidak@equinix.com>
2024-10-26 18:33:30 -0500

    1.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,33 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.30
 #
 # opens portfolio.csv, reads all the lines and calculates how much it
 # cost to purchase all the shares
 # expected output:
 # Total cost 44671.15
-total_cost = 0.0
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for l in f:
-        name, shares, price = l.split(',')
-        total_cost += int(shares) * float(price)
-print("Total cost", total_cost)
 
+import csv
+import sys
+
+def portfolio_cost(filename):
+    total_cost = 0.0
+    f = open(filename, 'rt')
+    rows = csv.reader(f)
+    headers = next(rows)
+    for r in rows:
+        name, shares, price = r
+        try:
+            total_cost += int(shares) * float(price)
+        except ValueError:
+            print("couldn't parse line:", r, "skipping")
+
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
