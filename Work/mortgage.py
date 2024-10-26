d0aead2f8b55a984317e147ac091ef90a51944dd
Alexander J. Maidak <amaidak@equinix.com>
2024-10-26 10:48:22 -0500

    1.4

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,8 +6,8 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 extra_payment_start_month = 1
-extra_payment_end_month = 1
-extra_payment = 0 
+extra_payment_end_month = 12
+extra_payment = 1000
 month = 0
 
 while principal > 0:
@@ -23,8 +23,8 @@ while principal > 0:
     principal = principal * (1+rate/12) - this_payment
         
     total_paid = total_paid + this_payment
-    print(month, round(total_paid,2), round(principal,2))
+    print(f'{month:>3.0f} {round(total_paid,2):10.2f} {round(principal,2):10.2f}')
     
     
-print('Total paid', round(total_paid,2))
-print('Months', month)
+print(f'Total paid: {round(total_paid,2):14.2f}')
+print(f'Months: {month:18.0f}')
